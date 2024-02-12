f185d0417b4917fd4156bea667a72f70de720159
A1aaAhmed <81260750+A1aaAhmed@users.noreply.github.com>
2024-02-12 14:50:14 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,190 +1,231 @@
 #!/usr/bin/python3
-"""Defines unittests for models/state.py.
-
-Unittest classes:
-    TestStateInstantiation
-    TestStateSave
-    TestStateToDict
 """
-
-import os
-import models
+This module is designed to test the State model
+"""
 import unittest
+import os
+from unittest.mock import patch
 from datetime import datetime
-from time import sleep
+from io import StringIO
+import uuid
+import models.base_model
 from models.state import State
 
 
-class TestStateInstantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the State class."""
+class TestState(unittest.TestCase):
+    """
+    Class to define the unittest
+    """
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(type(State()), State)
+    def test_State(self):
+        """
+        Test the StateModel class
+        """
+        b = State()
+        self.assertIsInstance(b, models.base_model.BaseModel)
+        self.assertTrue(issubclass(type(b), models.base_model.BaseModel))
+        with patch('models.base_model.uuid4') as mock_id:
+            mock_id.return_value = str(
+                uuid.UUID("b6a6e15c-c67d-4312-9a75-9d084935e579"))
 
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(State(), models.storage.all().values())
+            base = State()
+            self.assertEqual(base.id, "b6a6e15c-c67d-4312-9a75-9d084935e579")
 
-    def test_id_is_public_str(self):
-        self.assertEqual(type(State().id), str)
+        with patch('models.base_model.datetime') as mock_date:
+            mock_date.now.return_value = datetime(2022, 8, 7, 19, 2, 19, 10000)
+            mock_date.side_effect = lambda *args, **kw: datetime(*args, **kw)
 
-    def test_created_at_is_public_datetime(self):
-        self.assertEqual(type(State().created_at), datetime)
+            base = State()
+            self.assertEqual(base.created_at, datetime(
+                2022, 8, 7, 19, 2, 19, 10000))
+            self.assertEqual(type(base.created_at), datetime)
+            self.assertEqual(base.updated_at, datetime(
+                2022, 8, 7, 19, 2, 19, 10000))
+            self.assertEqual(type(base.updated_at), datetime)
 
-    def test_updated_at_is_public_datetime(self):
-        self.assertEqual(type(State().updated_at), datetime)
+        with patch('models.base_model.uuid4') as mock_id:
+            with patch('models.base_model.datetime') as mock_date:
+                mock_id.return_value = str(
+                    uuid.UUID("4c977185-d3f7-4aaa-a46f-c9d27ec4bd5e"))
+                mock_date.now.return_value = datetime(
+                    2022, 8, 7, 19, 2, 19, 10000)
+                mock_date.side_effect = lambda *args, **kw: datetime(
+                    *args, **kw)
+                b1 = State('foo')
+                self.assertEqual(b1.id, "4c977185-d3f7-4aaa-a46f-c9d27ec4bd5e")
+                self.assertEqual(b1.created_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.created_at), datetime)
+                self.assertEqual(b1.updated_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.updated_at), datetime)
 
-    def test_name_is_public_class_attribute(self):
-        st = State()
-        self.assertEqual(type(State.name), str)
-        self.assertIn("name", dir(st))
-        self.assertNotIn("name", st.__dict__)
+                b1 = State(2)
+                self.assertEqual(b1.id, "4c977185-d3f7-4aaa-a46f-c9d27ec4bd5e")
+                self.assertEqual(b1.created_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.created_at), datetime)
+                self.assertEqual(b1.updated_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.updated_at), datetime)
 
-    def test_two_states_unique_ids(self):
-        st1 = State()
-        st2 = State()
-        self.assertNotEqual(st1.id, st2.id)
+                b1 = State({})
+                self.assertEqual(b1.id, "4c977185-d3f7-4aaa-a46f-c9d27ec4bd5e")
+                self.assertEqual(b1.created_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.created_at), datetime)
+                self.assertEqual(b1.updated_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.updated_at), datetime)
 
-    def test_two_states_different_created_at(self):
-        st1 = State()
-        sleep(0.05)
-        st2 = State()
-        self.assertLess(st1.created_at, st2.created_at)
+                b1 = State(2, 'foo')
+                self.assertEqual(b1.id, "4c977185-d3f7-4aaa-a46f-c9d27ec4bd5e")
+                self.assertEqual(b1.created_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.created_at), datetime)
+                self.assertEqual(b1.updated_at, datetime(
+                    2022, 8, 7, 19, 2, 19, 10000))
+                self.assertEqual(type(b1.updated_at), datetime)
 
-    def test_two_states_different_updated_at(self):
-        st1 = State()
-        sleep(0.05)
-        st2 = State()
-        self.assertLess(st1.updated_at, st2.updated_at)
+        clsdict = {"id": "56d43177-cc5f-4d6c-a0c1-e167f8c27337",
+                   "created_at": "2017-09-28T21:05:54.119434",
+                   "updated_at": "2017-09-28T21:05:54.119434",
+                   "__class__": "State"}
+        b2 = State(**clsdict)
+        self.assertEqual(b2.id, "56d43177-cc5f-4d6c-a0c1-e167f8c27337")
+        self.assertEqual(b2.created_at, datetime(
+            2017, 9, 28, 21, 5, 54, 119434))
+        self.assertEqual(type(b1.created_at), datetime)
+        self.assertEqual(b2.updated_at, datetime(
+            2017, 9, 28, 21, 5, 54, 119434))
+        self.assertEqual(type(b2.updated_at), datetime)
 
-    def test_str_representation(self):
-        dt = datetime.today()
-        dt_repr = repr(dt)
-        st = State()
-        st.id = "123456"
-        st.created_at = st.updated_at = dt
-        st_str = str(st)
-        self.assertIn("[State] (123456)", st_str)
-        self.assertIn("'id': '123456'", st_str)
-        self.assertIn("'created_at': " + dt_repr, st_str)
-        self.assertIn("'updated_at': " + dt_repr, st_str)
+        clsdict = {"id": "56d43177-cc5f-4d6c-a0c1-e167f8c27337",
+                   "created_at": "2017-09-28T21:05:54.119434",
+                   "__class__": "State"}
+        b2 = State(**clsdict)
+        self.assertEqual(b2.id, "56d43177-cc5f-4d6c-a0c1-e167f8c27337")
+        self.assertEqual(b2.created_at, datetime(
+            2017, 9, 28, 21, 5, 54, 119434))
+        self.assertEqual(type(b1.created_at), datetime)
+        with self.assertRaises(AttributeError):
+            print(b2.updated_at)
 
-    def test_args_unused(self):
-        st = State(None)
-        self.assertNotIn(None, st.__dict__.values())
+        clsdict = {"id": "56d43177-cc5f-4d6c-a0c1-e167f8c27337",
+                   "__class__": "State"}
+        b2 = State(**clsdict)
+        self.assertEqual(b2.id, "56d43177-cc5f-4d6c-a0c1-e167f8c27337")
+        with self.assertRaises(AttributeError):
+            print(b2.created_at)
+        with self.assertRaises(AttributeError):
+            print(b2.updated_at)
 
-    def test_instantiation_with_kwargs(self):
-        dt = datetime.today()
-        dt_iso = dt.isoformat()
-        st = State(id="345", created_at=dt_iso, updated_at=dt_iso)
-        self.assertEqual(st.id, "345")
-        self.assertEqual(st.created_at, dt)
-        self.assertEqual(st.updated_at, dt)
+    @ patch('sys.stdout', new_callable=StringIO)
+    def test_str(self, stdout):
+        """
+        Test the State class __str__ method
+        """
+        with patch('models.base_model.uuid4') as mock_id:
+            with patch('models.base_model.datetime') as mock_date:
+                mock_id.return_value = str(
+                    uuid.UUID("b6a6e15c-c67d-4312-9a75-9d084935e579"))
+                mock_date.now.return_value = datetime(
+                    2017, 9, 28, 21, 5, 54, 119434)
+                mock_date.side_effect = lambda *args, **kw: datetime(
+                    *args, **kw)
+                base = State()
+                print(base)
+                expted_out = ("[State] "
+                              "(b6a6e15c-c67d-4312-9a75-9d084935e579) "
+                              "{'id': 'b6a6e15c-c67d-4312-9a75-9d08493"
+                              "5e579', 'created_at': datetime.datetime"
+                              "(2017, 9, 28, 21, 5, 54, 119434), "
+                              "'updated_at': datetime.datetime(2017, "
+                              "9, 28, 21, 5, 54, 119434)}\n")
+                self.assertEqual(stdout.getvalue(), expted_out)
+                stdout.truncate(0)
+                stdout.seek(0)
 
-    def test_instantiation_with_None_kwargs(self):
+    def test_save(self):
+        """
+        Test the State class save method
+        """
+        PATH = 'file.json'
+        with patch('models.base_model.uuid4') as mock_id:
+            with patch('models.base_model.datetime') as mock_date:
+                mock_id.return_value = str(
+                    uuid.UUID("788f5f32-d874-4387-872c-e925314ba80a"))
+                mock_date.now.return_value = datetime(
+                    2017, 9, 28, 21, 5, 54, 119434)
+                mock_date.side_effect = lambda *args, **kw: datetime(
+                    *args, **kw)
+                base = State()
+                self.assertEqual(base.updated_at, datetime(
+                    2017, 9, 28, 21, 5, 54, 119434))
+                mock_date.now.return_value = datetime(
+                    2017, 9, 28, 21, 5, 54, 119572)
+                base.save()
+                self.assertEqual(base.updated_at, datetime(
+                    2017, 9, 28, 21, 5, 54, 119572))
+                self.assertEqual(os.path.isfile(
+                    PATH) and os.access(PATH, os.R_OK), True)
+                cont = self.write_file(PATH)
+                expected = ('"State.788f5f32-d874-4387-872c-e925314ba80a":'
+                            ' {"id": "788f5f32-d874-4387-872c-e925314ba80a", '
+                            '"created_at": "2017-09-28T21:05:54.119434", '
+                            '"updated_at": "2017-09-28T21:05:54.119572", '
+                            '"__class__": "State"}')
+                self.assertIn(expected, cont)
         with self.assertRaises(TypeError):
-            State(id=None, created_at=None, updated_at=None)
-
-
-class TestStateSave(unittest.TestCase):
-    """Unittests for testing save method of the State class."""
-
-    @classmethod
-    def setUpClass(cls):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-
-    def test_one_save(self):
-        st = State()
-        sleep(0.05)
-        first_updated_at = st.updated_at
-        st.save()
-        self.assertLess(first_updated_at, st.updated_at)
-
-    def test_two_saves(self):
-        st = State()
-        sleep(0.05)
-        first_updated_at = st.updated_at
-        st.save()
-        second_updated_at = st.updated_at
-        self.assertLess(first_updated_at, second_updated_at)
-        sleep(0.05)
-        st.save()
-        self.assertLess(second_updated_at, st.updated_at)
-
-    def test_save_with_arg(self):
-        st = State()
+            base.save(2)
         with self.assertRaises(TypeError):
-            st.save(None)
+            base.save('foo')
+        os.remove(PATH)
 
-    def test_save_updates_file(self):
-        st = State()
-        st.save()
-        st_id = "State." + st.id
-        with open("file.json", "r") as f:
-            self.assertIn(st_id, f.read())
+    def test_to_dict(self):
+        """
+        Test the State class to_dict method
+        """
+        with patch('models.base_model.uuid4') as mock_id:
+            with patch('models.base_model.datetime') as mock_date:
+                mock_id.return_value = str(
+                    uuid.UUID("07331301-1393-4f7f-8da5-1f9be6216ad4"))
+                mock_date.now.return_value = datetime(
+                    2017, 9, 28, 21, 5, 54, 119434)
+                mock_date.side_effect = lambda *args, **kw: datetime(
+                    *args, **kw)
+                base = State()
+                clsdict = {"id": "07331301-1393-4f7f-8da5-1f9be6216ad4",
+                           "created_at": "2017-09-28T21:05:54.119434",
+                           "updated_at": "2017-09-28T21:05:54.119434",
+                           "__class__": "State"}
+                self.assertEqual(base.to_dict(), clsdict)
 
-
-class TestStateToDict(unittest.TestCase):
-    """Unittests for testing to_dict method of the State class."""
-
-    def test_to_dict_type(self):
-        self.assertIsInstance(State().to_dict(), dict)
-
-    def test_to_dict_contains_correct_keys(self):
-        st = State()
-        self.assertIn("id", st.to_dict())
-        self.assertIn("created_at", st.to_dict())
-        self.assertIn("updated_at", st.to_dict())
-        self.assertIn("__class__", st.to_dict())
-
-    def test_to_dict_contains_added_attributes(self):
-        st = State()
-        st.middle_name = "Holberton"
-        st.my_number = 98
-        self.assertEqual("Holberton", st.middle_name)
-        self.assertIn("my_number", st.to_dict())
-
-    def test_to_dict_datetime_attributes_are_strs(self):
-        st = State()
-        st_dict = st.to_dict()
-        self.assertIsInstance(st_dict["id"], str)
-        self.assertIsInstance(st_dict["created_at"], str)
-        self.assertIsInstance(st_dict["updated_at"], str)
-
-    def test_to_dict_output(self):
-        dt = datetime.today()
-        st = State()
-        st.id = "123456"
-        st.created_at = st.updated_at = dt
-        tdict = {
-            'id': '123456',
-            '__class__': 'State',
-            'created_at': dt.isoformat(),
-            'updated_at': dt.isoformat(),
-        }
-        self.assertDictEqual(st.to_dict(), tdict)
-
-    def test_contrast_to_dict_dunder_dict(self):
-        st = State()
-        self.assertNotEqual(st.to_dict(), st.__dict__)
-
-    def test_to_dict_with_arg(self):
-        st = State()
         with self.assertRaises(TypeError):
-            st.to_dict(None)
+            base.to_dict(2)
+        with self.assertRaises(TypeError):
+            base.to_dict('foo')
 
+    def test_attrib(self):
+        """
+        Test the State class public attributes
+        """
+        b = State()
+        self.assertTrue(hasattr(b, 'name'))
+        self.assertEqual(b.name, "")
+        self.assertEqual(type(b.name), str)
 
-if __name__ == "__main__":
-    unittest.main()
+    @staticmethod
+    def write_file(filename):
+        """
+        A function that opens and reads a file
+        Args:
+            filename (str)
+        Returns:
+            number of characters written into file
+        """
+        with open(filename, 'r', encoding="utf-8") as f:
+            text = ""
+            for line in f:
+                text += line
+            return text
