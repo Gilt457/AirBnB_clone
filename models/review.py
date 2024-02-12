54cdf38043a2963b7473907ceda95d40c21923ad
A1aaAhmed <81260750+A1aaAhmed@users.noreply.github.com>
2024-02-12 15:09:33 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""Module for Review class"""
 
-import models.base_model as bm
+"""
+A module that defines a Review class which inherits BaseModel
+"""
+
+from models.base_model import BaseModel
 
 
-class Review(bm.BaseModel):
-    """Class to handle review data"""
+class Review(BaseModel):
+    """
+    Defines all common attribute/methods for other classes
 
-    text = ""
-    user_id = ""
+    Attr:
+        id (str)
+        created_at (datetime)
+        updated_at (datetime)
+        place_id (str)
+        user_id (str)
+        text (str)
+    """
     place_id = ""
+    user_id = ""
+    text = ""
